2bd60290972d79e4b1afbb8be9e0b5bcd71804be
Max Williams <max.williams@deliveryhero.com>
2018-11-05 12:29:38 +0100

    initial commit after copying files from other repo

---
+++ examples/postgresql/main.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "azs" {
+  type = "list"
+
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+}
+
+module "aurora" {
+  source                          = "../../"
+  name                            = "aurora-example"
+  engine                          = "aurora-postgresql"
+  engine_version                  = "9.6.3"
+  subnets                         = ["${module.vpc.database_subnets}"]
+  availability_zones              = ["${var.azs}"]
+  vpc_id                          = "${module.vpc.vpc_id}"
+  replica_count                   = 1
+  instance_type                   = "db.r4.large"
+  apply_immediately               = true
+  skip_final_snapshot             = true
+  db_parameter_group_name         = "${aws_db_parameter_group.aurora_db_postgres96_parameter_group.id}"
+  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.aurora_cluster_postgres96_parameter_group.id}"
+}
+
+resource "aws_db_parameter_group" "aurora_db_postgres96_parameter_group" {
+  name        = "test-aurora-db-postgres96-parameter-group"
+  family      = "aurora-postgresql9.6"
+  description = "test-aurora-db-postgres96-parameter-group"
+}
+
+resource "aws_rds_cluster_parameter_group" "aurora_cluster_postgres96_parameter_group" {
+  name        = "test-aurora-postgres96-cluster-parameter-group"
+  family      = "aurora-postgresql9.6"
+  description = "test-aurora-postgres96-cluster-parameter-group"
+}
+
+resource "aws_security_group" "app_servers" {
+  name        = "app-servers"
+  description = "For application servers"
+  vpc_id      = "${module.vpc.vpc_id}"
+}
+
+resource "aws_security_group_rule" "allow_access" {
+  type                     = "ingress"
+  from_port                = "${module.aurora.this_rds_cluster_port}"
+  to_port                  = "${module.aurora.this_rds_cluster_port}"
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.app_servers.id}"
+  security_group_id        = "${module.aurora.this_security_group_id}"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "example"
+  cidr   = "10.0.0.0/16"
+  azs    = ["${var.azs}"]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/25",
+  ]
+
+  public_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/25",
+  ]
+
+  database_subnets = [
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+    "10.0.9.0/25",
+  ]
+}
---
+++ outputs.tf
@@ -0,0 +1,39 @@
+output "this_rds_cluster_id" {
+  description = "The ID of the cluster"
+  value       = "${aws_rds_cluster.this.id}"
+}
+
+output "this_rds_cluster_endpoint" {
+  description = "The cluster endpoint"
+  value       = "${aws_rds_cluster.this.endpoint}"
+}
+
+output "this_rds_cluster_reader_endpoint" {
+  description = "The cluster reader endpoint"
+  value       = "${aws_rds_cluster.this.reader_endpoint}"
+}
+
+output "this_rds_cluster_master_password" {
+  description = "The master password"
+  value       = "${aws_rds_cluster.this.master_password}"
+}
+
+output "this_rds_cluster_port" {
+  description = "The port"
+  value       = "${aws_rds_cluster.this.port}"
+}
+
+output "this_rds_cluster_master_username" {
+  description = "The master username"
+  value       = "${aws_rds_cluster.this.master_username}"
+}
+
+output "this_rds_cluster_instance_endpoints" {
+  description = "A list of all cluster instance endpoints"
+  value       = ["${aws_rds_cluster_instance.this.*.endpoint}"]
+}
+
+output "this_security_group_id" {
+  description = "The security group ID of the cluster"
+  value       = "${aws_security_group.this.id}"
+}
