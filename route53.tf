da879bc030224bd53d863c788fa140f250093e2f
Brenno Oliveira <brenno@bsd.com.br>
2019-04-03 20:53:22 +0200

    add DNS record for the reader endpoint

--- route53.tf
+++ route53.tf
@@ -6,3 +6,12 @@ resource "aws_route53_record" "default" {
   ttl     = "${var.route53_record_ttl}"
   records = ["${aws_rds_cluster.main.endpoint}"]
 }
+
+resource "aws_route53_record" "reader" {
+  count   = "${var.create_resources ? var.route53_zone_id == "" ? 0 : 1 : 0}"
+  zone_id = "${var.route53_zone_id}"
+  name    = "${aws_rds_cluster.main.id}${var.reader_endpoint_suffix}${var.route53_record_appendix}"
+  type    = "CNAME"
+  ttl     = "${var.route53_record_ttl}"
+  records = ["${aws_rds_cluster.main.reader_endpoint}"]
+}
