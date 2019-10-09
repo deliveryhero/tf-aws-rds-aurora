710cc3c1198f97fecd37bc49f0da1bb3dddeb997
oliver.klette <oliver.klette@deliveryhero.com>
2019-10-09 18:35:00 +0200

    Upgrade module to support Terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,35 @@
 output "cluster_id" {
   description = "The ID of the cluster"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.id)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.id, [""])[0]
 }
 
 output "cluster_endpoint" {
   description = "The cluster endpoint"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.endpoint)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.endpoint, [""])[0]
 }
 
 output "cluster_reader_endpoint" {
   description = "The cluster reader endpoint"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.reader_endpoint)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.reader_endpoint, [""])[0]
 }
 
 output "cluster_master_username" {
   description = "The master username"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.master_username)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.master_username, [""])[0]
 }
 
 output "cluster_master_password" {
   description = "The master password"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.master_password)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.master_password, [""])[0]
 }
 
 output "cluster_port" {
   description = "The port"
-  value       = "${element(split(",", join(",", aws_rds_cluster.main.*.port)), 0)}"
+  value       = concat(aws_rds_cluster.main.*.port, [""])[0]
 }
 
 output "security_group_id" {
   description = "The security group ID of the cluster"
-  value       = "${element(split(",", join(",", aws_security_group.main.*.id)), 0)}"
+  value       = concat(aws_security_group.main.*.id, [""])[0]
 }
+
