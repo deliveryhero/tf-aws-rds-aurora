084f2647bef77d61cafcbd88df8100876d960206
Muhammad Salman <muhammad.salman@deliveryhero.com>
2022-04-26 19:03:10 +0200

    fix: add ssm resource to global create condition and add pre commit config

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "random_id" "master_password" {
 }
 
 resource "aws_ssm_parameter" "superuser_password" {
-  count = var.store_master_creds_ssm ? 1 : 0
+  count     = var.create_resources && var.store_master_creds_ssm ? 1 : 0
   name      = "${var.prefix_master_creds_ssm}/${aws_rds_cluster.main[0].endpoint}/superuser/password"
   type      = "SecureString"
   value     = local.master_password
@@ -19,7 +19,7 @@ resource "aws_ssm_parameter" "superuser_password" {
 }
 
 resource "aws_ssm_parameter" "superuser_name" {
-  count = var.store_master_creds_ssm ? 1 : 0
+  count     = var.create_resources && var.store_master_creds_ssm ? 1 : 0
   name      = "${var.prefix_master_creds_ssm}/${aws_rds_cluster.main[0].endpoint}/superuser/name"
   type      = "SecureString"
   value     = var.username
